4da573183eaee69157f4e11d05e6e94709862ca2
Sigfrid <sigfridgom@gmail.com>
2019-07-22 16:58:17 -0600

    cambios

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -18,6 +18,28 @@
 
 </head>
 <body>
+	<nav class="navbar navbar-expand-sm navbar-dark bg-info" >
+		<a class="navbar-brand font-weight-bold" href="<?= base_url()?>">OpenHouse</a>
+		<button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+			aria-expanded="false" aria-label="Toggle navigation"></button>
+		<div class="collapse navbar-collapse" id="collapsibleNavId">
+			<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+				<li class="nav-item active">
+					<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Link</a>
+				</li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+					<div class="dropdown-menu" aria-labelledby="dropdownId">
+						<a class="dropdown-item" href="#">Action 1</a>
+						<a class="dropdown-item" href="#">Action 2</a>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</nav>
 	<div class="container-fluid" id="main-content">
 
 	</div>
